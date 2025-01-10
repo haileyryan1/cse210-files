79a93e2fe07865440d8bc45168990973f05f4029
Hailey Ryan <hailey.ryan1@gmail.com>
2025-01-10 12:18:44 -0700

    C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        string letterGrade = "Start";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }   
+        else if (percentage >=80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentage >=70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentage >=60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Grade: {letterGrade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry! You failed the class, better luck next time.");
+        }
     }
 }
