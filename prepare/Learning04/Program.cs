8d7a8dc05076ca31812bc3ef6f5c97b972222700
Hailey Ryan <hailey.ryan1@gmail.com>
2025-02-18 13:10:21 -0700

    Learning04 Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment basicAssign = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(basicAssign.GetSummary());
+
+        MathAssignment mathAssign = new MathAssignment("Roberto Rodriguez", "Fractions","7.3","8-19");
+        Console.WriteLine(mathAssign.GetSummary());
+        Console.WriteLine(mathAssign.GetHomeworkList());
+
+        WritingAssignment writingAssign = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssign.GetSummary());
+        Console.WriteLine(writingAssign.GetWritingInfo());
     }
 }
